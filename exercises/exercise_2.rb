2e8cec39e4b3fe0f1920a965b10da9da8c0c4563
egomatsushita <eduardo.go.rj@gmail.com>
2017-08-01 16:15:26 +0000

    Conclude exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,8 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+@store1.update(name: 'North Vancouver')
+
+puts "Update name of store 1 to #{@store1.name}"
