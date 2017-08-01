047485a4818cc5392a7b41cf6c621f4a40d43e89
egomatsushita <eduardo.go.rj@gmail.com>
2017-08-01 18:01:55 +0000

    Conclude exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,45 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Employee < ActiveRecord::Base
+  validates :first_name,
+            presence: true
+
+  validates :last_name,
+            presence: true
+
+  validates :hourl_rate,
+            numericality: true,
+            length: {minimum: 40, maximum: 200}
+
+  validates :store_id,
+            presence: true
+
+end
+
+class Store < ActiveRecord::Base
+  validates :name,
+            presence: true,
+            length: {minimum: 3}
+
+  validates :annual_revenue,
+            numericality: true,
+            length: {minimum: 0}
+
+  validate :carry_mens_or_womens_apparel,
+            on: :create
+
+  def carry_mens_or_womens_apparel
+    unless :mens_apparel == false && :womens_apparel == false
+      errors.add("Sorry, ", "there should be at least one of the men's or women's apparel.")
+    end
+  end
+end
+
+print "Enter a store name: "
+user_store = $stdin.gets.chomp
+store = Store.create(name: user_store)
+puts store.errors.full_messages
+
+
